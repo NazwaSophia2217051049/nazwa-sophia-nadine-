2ed5bc53eb03158e03ec1b2b5eadd42da53d50bd
NazwaSophia2217051049 <112567851+NazwaSophia2217051049@users.noreply.github.com>
2022-09-02 09:17:13 +0700

    Add files via upload

---
+++ nazwa sophia 2217051049.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string a;
+	string b;
+	string c;
+	string d;
+	cout<<"Nama : ";
+	getline(cin,a);
+	
+	cout<<"NPM : ";
+	cin>>b;
+	
+	cout<<"Alamat : ";
+	cin>>c;
+	
+	cout<<"Hobi : ";
+	cin>>d;
+	
+	cout<<"Nama saya adalah : "<<a<<endl;
+	cout<<"Npm saya adalah : "<<b<<endl;
+	cout<<"Alamat saya adalah : "<<c<<endl;
+	cout<<"Hobi saya adalah : "<<d<<endl;
+	
+
+	return 0;	
+}
+
+
+
+
